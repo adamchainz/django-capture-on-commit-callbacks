1c179f2ebe66508fb09a60d6b282f304edf10926
Adam Johnson <me@adamj.eu>
2021-09-28 17:27:43 +0100

    settings

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,8 @@
 import os
 from typing import List
 
+import django
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = "NOTASECRET"
@@ -26,4 +28,8 @@ ROOT_URLCONF = "tests.urls"
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
-USE_L10N = True
+
+if django.VERSION < (4, 0):
+    USE_L10N = True
+
+USE_TZ = True
